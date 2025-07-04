3a088b5f2eb961e10caa32c9ace5e80d411f4e2c
Adeboss <ademolaadeigbe3@gmail.com>
2025-07-04 09:38:04 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
    <>
       <div className="w-full h-screen bg-black font-serif text-white flex justify-center align-middle p-3.5">
-        <h1 className="text-white p-3 align-middle">Welcome to Rome </h1>
+        <h1 className="text-white p-3 align-middle">Welcome to home </h1>
       </div>
    </>
   );
