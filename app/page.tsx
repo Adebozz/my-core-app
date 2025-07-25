f7f54d74b6c7f880aaf0d6a425fb0067c62afe27
Adeboss <ademolaadeigbe3@gmail.com>
2025-07-25 23:37:55 +0100

    `Added two new h1 elements to the Home component in page.tsx`

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,8 @@ export default function Home() {
   return (
    <>
       <div className="w-full h-screen bg-black font-serif text-white flex justify-center align-middle p-3.5">
-        <h1 className="text-white p-3 align-middle">Welcome to the home of code </h1>
+        <h1 className="text-white p-3 align-middle">Welcome to the home of code , Take sroll</h1>
+        <h1 className="text-white p-3 align-middle">Welcome to the home of code , Take a</h1>
       </div>
    </>
   );
